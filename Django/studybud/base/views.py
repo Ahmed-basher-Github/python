d9c203f91a84b8161ebf1872d0f6b136591cb92f
Ahmed Khair <basherahmed362@gmail.com>
2025-02-05 03:49:32 +0400

    database, and query

--- Django/studybud/base/views.py
+++ Django/studybud/base/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Room
 # Create your views here.
-
+rooms = []
 def home(request):
-    return render(request, 'base/home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', {'rooms': rooms})
 def about(request):
     return render(request, 'base/about.html')
-def room(request):
-    return render(request, 'base/room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
